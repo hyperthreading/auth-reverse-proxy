aba9e34d3e7fa75d0b3d73b2988de8d37543fd29
hyperthreading <no1expman@gmail.com>
2019-10-01 15:55:32 +0900

    Add signed cookie for auth

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+const fs = require('fs');
 
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-
-const onProxyRes = (proxyRes, req, res) => {
-  proxyRes.headers['access-control-allow-origin'] = '*';
+let config = {
+  target: 'http://localhost:3000',
+  secret: '123123',
+  allowedIps: ['::ffff:127.0.0.1', '::1', '127.0.0.1'],
+  port: 3001
 };
 
-// proxy middleware options
+if (fs.existsSync(path.resolve(process.cwd(), './config'))) {
+  config = require('./config');
+}
+
+const express = require('express');
+const morgan = require('morgan');
+const proxy = require('http-proxy-middleware');
+const cookieParser = require('cookie-parser');
+
 const options = {
-  target: process.env.PROXY_TARGET, // target host
-  changeOrigin: true, // needed for virtual hosted sites
+  target: config.target,
+  changeOrigin: true,
   secure: false,
-  logLevel: 'debug',
-  onProxyRes
 };
 
 const proxyHandler = proxy(options);
-
+const cookieName = 'auth_session';
 const app = express();
+
+app.use(cookieParser(config.secret));
+app.use(morgan('combined'));
 app.use('/', function(req, res, next) {
-  console.log(req.ip);
-  next();
+  if (req.signedCookies[cookieName]) {
+    const validCookie = Number(req.signedCookies[cookieName]) > new Date().getTime();
+    if (validCookie) {
+      next();
+      return;
+    }
+  }
+
+  if (config.allowedIps.includes(req.ip)) {
+    const expireAt = new Date().getTime() + 1000 * 60 * 60 * 24 * 3;
+
+    res.cookie(cookieName, expireAt, {
+      maxAge: 1000 * 60 * 60 * 24 * 3,
+      httpOnly: true,
+      sameSite: true,
+      signed: true,
+    });
+    next();
+    return;
+  }
+
+  res.status(401).send("Unauthorized");
+
 }, proxyHandler);
 
-app.listen(3001);
+app.listen(config.port);
